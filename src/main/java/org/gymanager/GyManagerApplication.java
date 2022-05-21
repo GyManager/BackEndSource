dc1ed36316ec6e6fa63cd9dffb2f7a87c6181762
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-21 12:00:55 -0300

    Initial Commits: Project base structure

---
+++ src/main/java/org/gymanager/GyManagerApplication.java
@@ -0,0 +1,11 @@
+package org.gymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GyManagerApplication.class, args);
+    }
+}
