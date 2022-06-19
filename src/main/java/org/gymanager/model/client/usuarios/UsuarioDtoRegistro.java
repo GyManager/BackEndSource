5f426d49d2e64ff30f9b43d751165d122bc8541e
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-06-19 11:39:56 -0300

    mas mejoras en el manejo de errores

--- src/main/java/org/gymanager/model/client/usuarios/UsuarioDtoRegistro.java
+++ src/main/java/org/gymanager/model/client/usuarios/UsuarioDtoRegistro.java
@@ -3,17 +3,21 @@ package org.gymanager.model.client.usuarios;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class UsuarioDtoRegistro {
-    @NotBlank(message = "El nombre del usuario es obligatorio")
+    @NotBlank(message = "El nombre del usuario es obligatorio.")
     private String nombre;
 
-    @NotBlank(message = "La contraseña es obligatoria")
+    @NotBlank(message = "La contraseña es obligatoria.")
+    @Size(min = 8, max = 25, message = "La contraseña debe tener entre 8 y 25 caracteres.")
     private String pass;
 
-    @NotBlank(message = "El mail es obligatorio")
+    @NotBlank(message = "El mail es obligatorio.")
+    @Email(message = "El mail ingresado debe ser un mail correctamente formado.")
     private String mail;
 }
