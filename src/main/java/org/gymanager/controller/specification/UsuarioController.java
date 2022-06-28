c342cd2263af6f8c86365e1e02b659c76cb0056a
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-06-28 20:39:24 -0300

    removiendo imports no usados

--- src/main/java/org/gymanager/controller/specification/UsuarioController.java
+++ src/main/java/org/gymanager/controller/specification/UsuarioController.java
@@ -1,10 +1,6 @@
 package org.gymanager.controller.specification;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
