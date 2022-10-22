8231477877882271fb72c116abb31685409da593
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-10-22 19:43:27 -0300

    fixing

--- src/main/java/org/gymanager/model/domain/Avatar.java
+++ src/main/java/org/gymanager/model/domain/Avatar.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,7 +14,6 @@ import javax.persistence.Table;
 public class Avatar {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUsuario;
     private String imagen;
 }
